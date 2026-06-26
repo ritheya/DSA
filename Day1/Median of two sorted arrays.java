f35ba1083e6c9b72ebe67143d2f5f7f1abf55b8d
ritheya <rithimahesh08@gmail.com>
2026-06-26 21:36:33 +0530

    Update Median of two sorted arrays.java

--- Day1/Median of two sorted arrays.java
+++ Day1/Median of two sorted arrays.java
@@ -1,6 +1,7 @@
-Problem: Median of two sorted arrays. Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+/*Problem: Median of two sorted arrays. Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 
 The overall run time complexity should be O(log (m+n)).
+*/
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
     int m=nums1.length;
